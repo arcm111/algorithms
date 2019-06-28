b9354a190083f11d5e4ddcefd004c26e2bb34b35
arcm111 <arcm111@hotmail.com>
2019-06-28 17:52:17 +0100

    improved printability

--- b-trees/Node.java
+++ b-trees/Node.java
@@ -1,53 +1,52 @@
 public class Node<T extends Comparable<T>, V> {
-	public int n;
-	public boolean isLeaf = true;
-	private final Array<Key<T, V>> keys;
-	private final Array<Node<T, V>> children;
+    public int n;
+    public boolean isLeaf = true;
+    private final Array<Key<T, V>> keys;
+    private final Array<Node<T, V>> children;
 
-	public Node(int minimumDegree) {
-		this.keys = new Array<>(2 * minimumDegree);
-		this.children = new Array<>(2 * minimumDegree);
-	}
+    public Node(int minimumDegree) {
+        this.keys = new Array<>(2 * minimumDegree);
+        this.children = new Array<>(2 * minimumDegree);
+    }
 
-	public Key<T, V> key(int ind) {
-		return keys.get(ind);
-	}
+    public Key<T, V> key(int ind) {
+        return keys.get(ind);
+    }
 
-	public void setKey(int i, Key<T, V> k) {
-		keys.set(i, k);
-	}
+    public void setKey(int i, Key<T, V> k) {
+        keys.set(i, k);
+    }
 
-	public Node<T, V> child(int ind) {
-		return children.get(ind);
-	}
+    public Node<T, V> child(int ind) {
+        return children.get(ind);
+    }
 
-	public void setChild(int i, Node<T, V> node) {
-		children.set(i, node);
-	}
+    public void setChild(int i, Node<T, V> node) {
+        children.set(i, node);
+    }
 
-	@Override
-	public String toString() {
-		if (n == 0) return "";
-		StringBuilder s = new StringBuilder();
-		System.out.println("Node[" + n + "]: isleaf = " + isLeaf);
-		for (int i = 0; i < n; i++) {
-			Key cur = keys.get(i);
-			if (cur == null) {
-				System.out.println("Error: key[" + i + "] is null. n = " + n);
-			} else {
-				s.append(cur.toString() + "\n");
-			}
-		}
-		if (!isLeaf) {
-			for (int i = 0; i <= n; i++) {
-				Node<T, V> child = children.get(i);
-				if (child == null) {
-					System.out.println("Error: child[" + i + "] is null");
-				} else {
-					s.append(child.toString());
-				}
-			}
-		}
-		return s.toString();
-	}
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        System.out.println("Node[" + n + "]: isleaf = " + isLeaf);
+        for (int i = 0; i < n; i++) {
+            s.append("key => ");
+            String txt = "null";
+            if (keys.get(i) != null) {
+                txt = keys.get(i).toString();
+            }
+            s.append(txt + "\n");
+        }
+        if (isLeaf) return s.toString();
+        for (int i = 0; i <= n; i++) {
+            s.append("child => ");
+            String txt = "null";
+            Node<T, V> child = children.get(i);
+            if (children.get(i) != null) {
+                txt = children.get(i).toString();
+            }
+            s.append(txt);
+        }
+        return s.toString();
+    }
 }
