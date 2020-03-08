097059b1de0291a2560599da7f30aad6b65b67b6
arcm111 <arcm111@hotmail.com>
2020-03-08 15:04:16 +0000

    added rabin-karp algorithm

--- selected-topics/string-matching/StringMatching.java
+++ selected-topics/string-matching/StringMatching.java
@@ -1,4 +1,10 @@
 import java.util.Arrays;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.math.BigInteger;
+import java.util.Random;
 
 public class StringMatching {
     public static void naiveStringMatcher(String T, String P) {
@@ -16,11 +22,49 @@ public class StringMatching {
         }
     }
 
+    public static void rabinKarpMatcher(String T, String P) {
+        int d = 256;
+        long q = BigInteger.probablePrime(24, new Random()).longValue();
+        int n = T.length();
+        int m = P.length();
+        long h = 1;
+        for (int i = 0; i < m - 1; i++) { // computes d^(m-1) % q
+            h = mod(h * d, q);
+        }
+        long p = 0;
+        long t = 0;
+        for (int i = 0; i < m; i++) {
+            p = mod(d * p + P.charAt(i), q);
+            t = mod(d * t + T.charAt(i), q);
+        }
+        for (int s = 0; s < n - m; s++) {
+            if (p == t) {
+                if (areEqual(T, P, s)) {
+                    System.out.println("Pattern occurs with shift: " + s);
+                }
+            }
+            if (s < n - m - 1) {
+                t = mod(d * (t - T.charAt(s) * h) + T.charAt(s + m), q);
+            }
+        }
+    }
+
+    private static long mod(long a, long b) {
+        return (a % b + b) % b;
+    }
+
+    private static boolean areEqual(String T, String P, int shift) {
+        for (int i = 0; i < P.length(); i++) {
+            if (P.charAt(i) != T.charAt(i + shift)) return false;
+        }
+        return true;
+    }
+
     public static void KMP_Matcher(String T, String P) {
         int n = T.length();
         int m = P.length();
         int[] pi = computePrefexFunction(P);
-        System.out.println("pi: " + Arrays.toString(pi));
+        System.out.println("Prefix function (pi): " + Arrays.toString(pi));
         int q = -1;
         for (int i = 0; i < n; i++) {
             while (q > 0 && P.charAt(q + 1) != T.charAt(i)) {
@@ -53,17 +97,32 @@ public class StringMatching {
         return pi;
     }
 
+    public static void finiteAutomatonMatcher(String T, String P) {
+        char[] charset = getCharSet(T);
+        int[][] delta = computeTransitionFunction(P, charset);
+        Map<Character, Integer> indexes = generateHashMap(charset);
+        int n = T.length();
+        int m = P.length();
+        int q = 0;
+        for (int i = 0; i < n; i++) {
+            q = delta[q][indexes.get(T.charAt(i))];
+            if (q == m) {
+                System.out.println("Pattern occurs with shift: " + (i - m + 1));
+            }
+        }
+    }
+
     private static int[][] computeTransitionFunction(String P, char[] Sigma) {
         int m = P.length();
         int n = Sigma.length;
-        int[][] delta = new int[m][n];
-        for (int q = 0; q < m; q++) {
+        int[][] delta = new int[m + 1][n];
+        for (int q = 0; q <= m; q++) {
             for (int i = 0; i < n; i++) {
                 char a = Sigma[i];
-                int k = Math.min(m, q + 1);
+                int k = Math.min(m + 1, q + 2);
                 do {
                     k--;
-                } while (!isSuffix(P, k, q, a));
+                } while (!isSuffix(P, k - 1, q - 1, a));
                 delta[q][i] = k;
             }
         }
@@ -71,10 +130,7 @@ public class StringMatching {
     }
 
     private static boolean isSuffix(String P, int k, int q, char a) {
-        if (k < 0 || q < 0 || k > q + 1) {
-            throw new IllegalArgumentException("invalid arguments");
-        }
-        if (k == 0) return true;
+        if (k == -1) return true;
         if (P.charAt(k--) != a) return false;
         while (k >= 0) {
             if (P.charAt(k--) != P.charAt(q--)) return false;
@@ -82,24 +138,46 @@ public class StringMatching {
         return true;
     }
 
+    private static char[] getCharSet(String t) {
+        Set<Character> set = new TreeSet<>();
+        for (char a : t.toCharArray()) set.add(new Character(a));
+        char[] res = new char[set.size()];
+        int i = 0;
+        for (Character c : set) res[i++] = c;
+        return res;
+    }
+
+    private static Map<Character, Integer> generateHashMap(char[] sigma) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < sigma.length; i++) {
+            map.put(sigma[i], i);
+        }
+        return map;
+    }
+
     public static void main(String[] args) {
         // Testing naive string matcher:
         String T = "acaabc";
         String P = "aab";
+        System.out.println("Naive: ");
         naiveStringMatcher(T, P);
         T = "bacbababaabcbabababacabcaabcbaababacabcaababbc"; // shifts 15, 30
         P = "ababaca";
+        System.out.println("Rabin-Karp: ");
+        rabinKarpMatcher(T, P);
+        System.out.println("KMP: ");
         KMP_Matcher(T, P);
         T = "aaaaaaaaaaaaaaaaa";
         P = "aaaaaaa";
         KMP_Matcher(T, P);
 
-        String a = "whatevermayhappenswhatever";
-        System.out.println(isSuffix(a, 7, 24, 'r'));
-        System.out.println(isSuffix("a", 1, 0, 'a'));
+        T = "bacbababaabcbabababacabcaabcbaababacabcaababbc"; // shifts 15, 30
         P = "ababaca";
         char[] s = {'a', 'b', 'c'};
         int[][] delta = computeTransitionFunction(P, s);
+        System.out.println("Finite-automaton-matcher: ");
+        System.out.println("Transition function (delta): ");
         for (int[] x : delta) System.out.println(Arrays.toString(x));
+        finiteAutomatonMatcher(T, P);
     }
 }
